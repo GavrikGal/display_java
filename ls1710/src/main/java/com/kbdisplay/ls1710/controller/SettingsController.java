5212c689c307280b67d77e1385617a2a66d93213
GavrikGal <gavrik.gal@gmail.com>
2015-06-03 19:42:53 -0300

    ls1710, добавление смены логина пользователя

--- ls1710/src/main/java/com/kbdisplay/ls1710/controller/SettingsController.java
+++ ls1710/src/main/java/com/kbdisplay/ls1710/controller/SettingsController.java
@@ -7,10 +7,13 @@ import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import com.kbdisplay.ls1710.domain.User;
 import com.kbdisplay.ls1710.service.data.UserService;
+import com.kbdisplay.ls1710.service.data.jpa.CustomUserDetails.CustomUserDetails;
 import com.kbdisplay.ls1710.view.settings.NormsSetting;
 import com.kbdisplay.ls1710.view.settings.UsersSetting;
 import com.kbdisplay.ls1710.view.settings.web.NormsSettingImpl;
@@ -22,6 +25,7 @@ public class SettingsController {
 	@Autowired
 	private UserService userService;
 
+	@PreAuthorize("hasRole('ROLE_USER')")
 	public UsersSetting newUsersSetting() {
 
 		UsersSetting usersSetting = new UsersSettingImpl();
@@ -41,39 +45,74 @@ public class SettingsController {
 		return normsSetting;
 	}
 
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	public User newUser() {
 		return new User();
 	}
 
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	public void saveUser(org.springframework.webflow.execution.RequestContext context) {
 		UsersSetting usersSetting = (UsersSetting) context.getFlowScope().get("usersSetting");
 
 		if (usersSetting != null) {
-			System.out.println(usersSetting.getSelected().getFirstName());
 			usersSetting.setSelected(userService.save(usersSetting.getSelected()));
 			usersSetting.getUsers().add(usersSetting.getSelected());
-			System.out.println(usersSetting.getSelected().getId());
 		}
 	}
 
 
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	public void deleteUser(User selected) {
 
 		FacesContext fc = FacesContext.getCurrentInstance();
-		fc.addMessage(null, new FacesMessage("Пользователь удален",
-				"Пользователь: " + selected.getLastName() + " "
-						+ selected.getFirstName() + " удален"));
 
-		ELContext elContext =
-				FacesContext.getCurrentInstance().getELContext();
-		UsersSetting usersSetting =
-				(UsersSetting) fc.getApplication().getELResolver()
-						.getValue(elContext, null, "usersSetting");
+		// TODO получаем зарегистированного пользователя
+		CustomUserDetails userDetails =
+				(CustomUserDetails) SecurityContextHolder.getContext()
+						.getAuthentication().getPrincipal();
+		User user = userDetails.getUsersDetails();
 
-		usersSetting.getUsers().remove(selected);
-		//userService.delete(selected);
+		if(selected.getId().equals(user.getId())) {
+			fc.addMessage(null, new FacesMessage("Пользователь не удален",
+					"Пользователь: " + selected.getLastName() + " "
+							+ selected.getFirstName() + " не может удалить сам себя"));
+		} else {
+			fc.addMessage(null, new FacesMessage("Пользователь удален",
+					"Пользователь: " + selected.getLastName() + " "
+							+ selected.getFirstName() + " удален"));
+
+			ELContext elContext =
+					FacesContext.getCurrentInstance().getELContext();
+			UsersSetting usersSetting =
+					(UsersSetting) fc.getApplication().getELResolver()
+							.getValue(elContext, null, "usersSetting");
+
+			usersSetting.getUsers().remove(selected);
+			//userService.delete(selected);
+		}
 	}
 
+	public User getCurrentUser() {
+		CustomUserDetails userDetails =
+				(CustomUserDetails) SecurityContextHolder.getContext()
+						.getAuthentication().getPrincipal();
+		User user = userDetails.getUsersDetails();
+
+		return user;
+	}
+
+	@PreAuthorize("hasRole('ROLE_USER')")
+	public void changeLogin(org.springframework.webflow.execution.RequestContext context) {
+		FacesContext fc = FacesContext.getCurrentInstance();
+
+		User user = (User) context.getFlowScope().get("currentUserDetails");
+		fc.addMessage(null, new FacesMessage("Логин изменен",
+				user.getLastName() + " "
+						+ user.getFirstName() + ", при следующем входе в систему, используйте новый логин"));
+		userService.save(user);
+	}
+
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	public String editUser(User selected) {
 		return null;
 	}
